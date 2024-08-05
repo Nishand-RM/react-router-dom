daf76a61f56cc0f2f751090deb11ca3c77211c1a
Nishand-RM <nishandrm9@gmail.com>
2024-08-05 12:47:03 +0530

    function component for counter inc,dec,reset

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,28 @@ const App = ()=>{
 
 //here useState returns 2 value i)name(name) ii)function(setName) created a destrucurring array and stored the useState return value in it.
     
-const [name,setName] = useState("Niki");
+const [count,setcount] = useState(0);
 
-function clickme(){
-    setName('Vijay')
+function increment(){
+    setcount(count + 1)
+}
+
+function decrement(){
+    setcount(count - 1)
+}
+
+function reset(){
+    setcount(0)
 }
 
     return(
         <div>
-            <h1>Hello {name}</h1>
-            <button type="button" class="btn btn-primary" onClick={clickme}>ChangeName</button>
+            <h1>Counter:{count}</h1>
+            <button type="button" class="btn btn-primary" onClick={increment}>INC</button>
+            <button type="button" class="btn btn-secondary" onClick={decrement}>DEC</button>
+            <button type="button" class="btn btn-success" onClick={reset}>Reset</button>
+
+
 
         </div>
     )
