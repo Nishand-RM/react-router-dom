e390a97db9f1d383997e4a42f86d4ebd5682bf1e
Nishand-RM <nishandrm9@gmail.com>
2024-08-05 11:21:38 +0530

    update value in state using setState()

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,42 @@
-//class component (stateful component)
-
-import Header from "./component/Header";
-import Description from "./component/Description";
-import { Component } from "react";
-import Hello from "./component/Hello";
-
-
-
-
+import {Component } from "react";
+import './App.css'
 
 class App extends Component{
-    render()
-    {
-        const name =["niki","vijay","Ajith"]
-        return(
-            <div>
-                <Hello 
-                name={name[0]} />
-                <Hello 
-                name ={name[1]}/>
-                
-            </div>
-        )
-    }
-}
 
+    constructor(){
+        //calling parent class constructor(component)
+        super();
+
+        //initialize a state
+        this.state = {
+            name:'niki',
+            age:23
+        }
+
+    }
+
+ clickme = ()=>{
+    /*should not mutate the state directly
+    this.state.name = "vishnu";
+    this.state.age = 24;*/
+    
+    //instead we have to use setState(): which helps in update the state along with our given sate.
+    this.setState({
+        name:"vishnu",
+        age:24
+    });
+
+ }
+    render(){
+          return(
+            <div className="box">
+            <h1>Hello {this.state.name}  !</h1>
+            <h2>what is your age: {this.state.age}</h2>
+            
+            <button type="button" class="btn btn-primary" onClick={this.clickme}>Change</button>
+            </div>
+          )
+          }
+}
 
 export default App;
--- src/component/Description.jsx
+++
@@ -1,9 +0,0 @@
-import { Component } from "react";
-
-class Description extends Component{
-    render(){
-       return <p>Nice to meet you {this.props.name}</p>
-    }
-}
-
-export default Description;
