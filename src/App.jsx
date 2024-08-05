11ca0d8b473fa95839399d680e4e8038f979c9ce
Nishand-RM <nishandrm9@gmail.com>
2024-08-05 13:19:29 +0530

    additional feature history added in functional component

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,23 @@ const App = ()=>{
 //here useState returns 2 value i)name(name) ii)function(setName) created a destrucurring array and stored the useState return value in it.
     
 const [count,setcount] = useState(0);
+const [clicks,setclick] = useState([]);
 
 function increment(){
     setcount(count + 1)
+    setclick([...clicks, 'INC'])
 }
 
 function decrement(){
     setcount(count - 1)
+    setclick([...clicks, 'DEC'])
+
 }
 
 function reset(){
     setcount(0)
+    setclick([...clicks, 'RST'])
+
 }
 
     return(
@@ -26,6 +32,7 @@ function reset(){
             <button type="button" class="btn btn-primary" onClick={increment}>INC</button>
             <button type="button" class="btn btn-secondary" onClick={decrement}>DEC</button>
             <button type="button" class="btn btn-success" onClick={reset}>Reset</button>
+            <p>History: {clicks.join(',')}</p>
 
 
 
